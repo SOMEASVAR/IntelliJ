26ca1ad184837db8b30153d6a558c6a5a4a33c8b
R.SOMEASVAR-212221230103 <93434149+SOMEASVAR@users.noreply.github.com>
2023-06-27 08:35:38 +0530

    Add files via upload

---
+++ src/main/java/com/sempratical/Sem/SemApplication.java
@@ -0,0 +1,13 @@
+package com.sempratical.Sem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sempratical/Sem/SemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sempratical.Sem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
